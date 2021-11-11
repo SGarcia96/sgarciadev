6623d3c7f017b74f16b2555fe451c9db2bed9404
sgarcia <sgcerverdadev@gmail.com>
2021-11-11 12:57:13 +0100

    add page transition (motion)

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,26 @@ import Layout from '../components/Layout/Layout'
 import Hero from '../components/Hero/Hero'
 import About from '../components/About/About'
 import Skills from '../components/Skills/Skills'
+import { motion } from 'framer-motion'
 
 const Home = () => {
   return (
     <Layout>
-      <Hero />
+      <motion.div initial="hidden" animate="visible" variants={{
+        hidden: {
+          scale: 0.8,
+          opacity: 0
+        },
+        visible: {
+          scale: 1,
+          opacity: 1,
+          transition: {
+            delay: 0.1
+          }
+        }
+      }}>
+        <Hero />
+      </motion.div>
       <About />
       <Skills />
     </Layout>
